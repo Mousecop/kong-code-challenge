a3b0c67f8614ade63981bdc1170768dbd9772a81
Jake Askew <joaskew13@gmail.com>
2022-06-03 13:28:45 -0400

    Setting up db and entities

---
+++ src/database/service.entity.ts
@@ -0,0 +1,26 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  Unique,
+  PrimaryGeneratedColumn,
+  Index,
+} from 'typeorm';
+
+@Entity('services')
+@Unique(['serviceId'])
+// Extending Base Entity to have out-of-the box methods such as `findAndCount`
+export class ServiceEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: number;
+
+  @Index()
+  @Column({ type: 'varchar', nullable: false })
+  serviceId: string;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column({ type: 'varchar' })
+  description: string;
+}
